a011cf6ec71d9a91aa7c577db3b1433a05b5816a
Gar Morley <garmorley@gmail.com>
2011-05-14 13:27:20 +0100

    add simple bashrc

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# bash config file
+ln -nfs ~/bin/dotfiles/bash/bashrc ~/.bashrc
+
 # vim config file and config directory
 ln -nfs ~/bin/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/bin/dotfiles/vim ~/.vim
