2b8e4b53f2277abacc5783df4221bf4397124565
Gar Morley <garmorley@gmail.com>
2011-05-15 19:49:50 +0100

    initial bash config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # bash config file
 ln -nfs ~/bin/dotfiles/bash/bashrc ~/.bashrc
+ln -nfs ~/bin/dotfiles/bash/bash_profile ~/.bash_profile
 
 # vim config file and config directory
 ln -nfs ~/bin/dotfiles/vim/vimrc ~/.vimrc
