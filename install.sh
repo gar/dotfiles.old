8b73ef573250c398c9d8eb092c8f780b5d1c7aa9
Gar Morley <garmorley@gmail.com>
2011-05-15 21:24:01 +0100

    add vcprompt to bash prompt

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 ln -nfs ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -nfs ~/.dotfiles/bash/bash_profile ~/.bash_profile
 
+# link bin directory
+ln -nfs ~/.dotfiles/bin ~/bin
+
 # vim config file and config directory
 ln -nfs ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/.dotfiles/vim ~/.vim
