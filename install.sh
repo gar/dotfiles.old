fb76cbca88107422d1debfba3a5837dfdd2e4ba1
Gar Morley <garmorley@gmail.com>
2011-05-21 16:17:26 +0100

    add concept of inputrc files

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # bash config file
 ln -nfs ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -nfs ~/.dotfiles/bash/bash_profile ~/.bash_profile
+ln -nfs ~/.dotfiles/bash/inputrc ~/.inputrc
 
 # link bin directory
 ln -nfs ~/.dotfiles/bin ~/bin
